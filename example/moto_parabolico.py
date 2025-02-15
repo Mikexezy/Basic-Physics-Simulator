20a94781a6c95bb4a0ee8e35e8860a94c94acd58
Mikexezy <michelepernaci65@gmail.com>
2025-02-16 00:49:23 +0100

    refactor: streamline main function by moving initialization code outside

--- example/moto_parabolico.py
+++ example/moto_parabolico.py
@@ -4,20 +4,16 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from core.env import *
 
-def main():
-    world = World(meters=100)
-    entities = [Entity(x=(10 * World.METERS_TO_PIXELS), y=(1 * World.METERS_TO_PIXELS), mass=2, trasversal_area=0.1, drag_coefficient=2, world=world)]
+world = World(meters=100)
+entities = [Entity(x=(10 * World.METERS_TO_PIXELS), y=(1 * World.METERS_TO_PIXELS), mass=2, trasversal_area=0.1, drag_coefficient=2, world=world)]
     
-    world.set_entities(entities)
-    world.setGravity(True)
-    world.setAirDrag(True)
+world.set_entities(entities)
+world.setGravity(True)
+world.setAirDrag(True)
     
-    esplosione = Force(magnitude=2000, alpha=math.radians(25), name="sparo")
+esplosione = Force(magnitude=2000, alpha=math.radians(25), name="sparo")
     
-    # non sto considerando la resistenza dell'aria e molte altre forze quindi il proiettile non perderà mai velocità orrizzontale
-    entities[0].addForce(esplosione, duration=0.05)
+# non sto considerando la resistenza dell'aria e molte altre forze quindi il proiettile non perderà mai velocità orrizzontale
+entities[0].addForce(esplosione, duration=0.05)
     
-    world.start()
-
-if __name__ == "__main__":
-    main()
+world.start()
--- example/sgancio_rifornimento_aereo.py
+++ example/sgancio_rifornimento_aereo.py
@@ -1,19 +1,16 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from core.env import *
 
-def main():
-    world = World(meters=1000)
-    entities = [Entity(x=(10 * World.METERS_TO_PIXELS), y=(500 * World.METERS_TO_PIXELS), mass=2, trasversal_area=0.2, drag_coefficient=0.1, world=world)]
+world = World(meters=1000)
+entities = [Entity(x=(10 * World.METERS_TO_PIXELS), y=(500 * World.METERS_TO_PIXELS), mass=2, trasversal_area=0.2, drag_coefficient=0.1, world=world)]
     
-    world.set_entities(entities)
-    world.setGravity(True)
-    world.setAirDrag(True)
+world.set_entities(entities)
+world.setGravity(True)
+world.setAirDrag(True)
     
-    entities[0].velocity_x = 200
+entities[0].velocity_x = 200
     
-    world.start()
-
-if __name__ == "__main__":
-    main()
+world.start()
